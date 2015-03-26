26e11ae8b82d49960cf74f58e0314ff89d2dea22
Alex Tkachenko <preusx.dev@gmail.com>
2015-03-26 19:41:15 +0200

    Theme files added to test runner. Test fixes.

--- test-runner.js
+++ test-runner.js
@@ -2,7 +2,7 @@ var testRunnerConfig = {
   describe: 'Stem stylus utils',
   stylus: {
     use: require('./index')(),
-    import: ['config', 'modules/utils']
+    import: ['config', 'modules/utils', 'theme']
   }
 }
 
